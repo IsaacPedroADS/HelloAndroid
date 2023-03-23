a3f740778756de8650e7809f177c0612baace349
Isaac Pedro <ispedrosi@gmail.com>
2023-03-22 22:43:17 -0300

    Modificação Teste 2

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
-// Teste 1
+// Teste 2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

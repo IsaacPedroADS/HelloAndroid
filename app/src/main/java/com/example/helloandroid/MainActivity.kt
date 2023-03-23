4ef613bb7b93aa5b71d852faafbdabcd1f9ead7d
IsaacPedroADS <105173709+IsaacPedroADS@users.noreply.github.com>
2023-03-22 23:14:42 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 // Comentário de alteracao 1.1
+// Comentário de alteracao 1.2
 // Teste 2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
